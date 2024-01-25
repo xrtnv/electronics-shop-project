c642581010c3e59e5f08819f91273ace66067033
xrtnv <xrtnv@xrtnv.com>
2024-01-25 13:42:29 +0300

    hw6

--- src/item.py
+++ src/item.py
@@ -1,6 +1,6 @@
-import csv
-import re
 from csv import DictReader
+
+from src.error import InstantiateCSVError
 
 
 class Item:
@@ -11,11 +11,10 @@ class Item:
     all = []
 
     def __repr__(self):
-        return f"Item('{self._name}', {self.price}, {self.quantity})"
-
+        return f"Item('{self.__name}', {self.price}, {self.quantity})"
 
     def __str__(self):
-        return self._name
+        return self.__name
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -25,22 +24,31 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self._name = name
+        if not name:
+            raise ValueError("Name cannot be empty.")
+        if not isinstance(quantity, int) or quantity < 0:
+            raise ValueError("Quantity must be a non-negative integer.")
+        if price <= 0:
+            raise ValueError("Price must be a positive number.")
+        self.__name = name[:10]
         self.price = price
         self.quantity = quantity
         self.all.append(self)
 
     @property
     def name(self):
-        return self._name
+        return self.__name
 
     @name.setter
-    def name(self, new_name: str):
-        if len(new_name) > 10:
-            new_name = new_name[0:10]
-        self._name = new_name
+    def name(self, new__name: str):
+        if len(new__name) > 10:
+            new__name = new__name[0:10]
+        self.__name = new__name
 
     def __add__(self, other):
+        if not isinstance(other, Item):
+            raise TypeError(
+                "Unsupported operand type(s) for +: '{}' and '{}'".format(type(self).__name__, type(other).__name__))
         return self.quantity + other.quantity
 
     def calculate_total_price(self) -> float:
@@ -68,18 +76,25 @@ class Item:
         self.price = round(self.price * self.pay_rate, 2)
 
     @classmethod
-    def instantiate_from_csv(cls, path: str):
+    def instantiate_from_csv(cls, path: str = "../src/items.csv"):
         """
         Инициализирует экземпляры класса из CSV-файла
         """
-        with open(path, 'r') as file:
-            csv_reader = csv.DictReader(file)
-            Item.all = []
-            for row in csv_reader:
-                _name = row['name']
-                price = cls.string_to_number(row['price'])
-                quantity = cls.string_to_number(row['quantity'])
-                cls(_name, price, quantity)
+        try:
+            with open(path, 'r') as file:
+                csv_reader = DictReader(file)
+                Item.all = []
+                for row in csv_reader:
+                    if None in row or '' in row:
+                        raise InstantiateCSVError(f"Файл {path} поврежден")
+                    if None in row.values() or ' ' in row.values():
+                        raise InstantiateCSVError(f"Файл {path} поврежден")
+                    __name = row['name']
+                    price = cls.string_to_number(row['price'])
+                    quantity = cls.string_to_number(row['quantity'])
+                    cls(__name, price, quantity)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Отсутствует файл {path}")
 
     @staticmethod
     def string_to_number(string):
